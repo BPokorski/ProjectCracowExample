24d244a7785c293980080ebb51a0606a23a91899
BPokorski <51976955+BPokorski@users.noreply.github.com>
2023-06-02 15:40:58 +0200

    Add files via upload

---
+++ Scripts/Character/UI/Inventory/ItemInventoryManagers/UIQuickItemInventoryManager.cs
@@ -0,0 +1,79 @@
+﻿using Character.Data;
+using Character.Inventory;
+using Character.UI.Inventory.ItemSlots;
+using Equipment;
+using UnityEngine;
+using Zenject;
+
+namespace Character.UI.Inventory.ItemInventoryManagers
+{
+    public class UIQuickItemInventoryManager : UIBaseItemInventoryManager
+    {
+        [Header("Slot data")]
+        [SerializeField] private ItemSlotsData _itemSlotsData;
+
+        private IQuickSlotInventoryManager _quickSlotInventoryManager;
+
+        [Inject]
+        private void Construct(IQuickSlotInventoryManager quickSlotInventoryManager,
+            UIQuickItemSlot.Factory quickItemSlotFactory)
+        {
+            _quickSlotInventoryManager = quickSlotInventoryManager;
+            _factory = quickItemSlotFactory;
+        }
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            InitItems(_itemSlotsData.QuickSlotNumber);
+            InitItemSlots();
+        }
+
+        private void OnEnable()
+        {
+            foreach (var itemSlot in _items)
+            {
+                itemSlot.ItemSlotActionTrigger += UpdateQuickItemSlot;
+            }
+            var items = GetItems();
+            ShowItems(items);
+        }
+
+        private void OnDisable()
+        {
+            foreach (var itemSlot in _items)
+            {
+                itemSlot.ItemSlotActionTrigger -= UpdateQuickItemSlot;
+            }
+            HideItems();
+        }
+        
+        private ItemSlot[] GetItems()
+        {
+            return _quickSlotInventoryManager.GetQuickItems();
+        }
+        
+        private void UpdateQuickItemSlot(int quickSlotID, ItemSlot currentItemSlot)
+        {
+            if (currentItemSlot != null)
+            {
+                var quickSlotPosition = _itemSlotIDPosition[quickSlotID];
+                _quickSlotInventoryManager.SetQuickSlotItem(quickSlotPosition, currentItemSlot);
+
+                for (int i = 0; i < _items.Length; i++)
+                {
+                    var quickItem = _items[i];
+
+                    var quickItemItemSlot = quickItem.CurrentItemSlot;
+                    if (quickItemItemSlot != null && 
+                        quickItemItemSlot.CurrentItem.ItemID == currentItemSlot.CurrentItem.ItemID &&
+                        i != quickSlotPosition)
+                    {
+                        quickItem.CurrentItemSlot = null;
+                        _quickSlotInventoryManager.RemoveQuickSLotItem(i);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Character/UI/Inventory/UIInventoryManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+// using Character.UI.Inventory.ItemSlot;
+using Character.UI.Inventory.ItemSlots;
+using Equipment;
+using Items;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace Character.UI.Inventory
+{
+    public class UIInventoryManager : MonoBehaviour
+    {
+        
+        [Header("Inventory")]
+        [SerializeField] private GameObject _inventoryUI;
+        [SerializeField] private UIMenuTabType _tabType;
+        
+        [SerializeField] private TMP_InputField _itemDescription;
+
+        private IUITabActivate _tabActivate;
+        [Inject]
+        private void Construct(IUITabActivate tabActivate)
+        {
+            _tabActivate = tabActivate;
+        }
+        
+        private void Awake()
+        {
+            
+            _tabType = UIMenuTabType.Inventory;
+            
+        }
+
+        private void OnEnable()
+        {
+            _tabActivate.TabActivate += OnTabActivate;
+            UIItemSlot.PointerEnter += ShowDescription;
+            UIItemSlot.PointerExit += HideDescription;
+        }
+
+        
+
+        private void OnDisable()
+        {
+            _tabActivate.TabActivate -= OnTabActivate;
+            UIItemSlot.PointerEnter -= ShowDescription;
+            UIItemSlot.PointerExit -= HideDescription;
+        }
+        
+        private void ShowDescription(ItemSlot itemSlot)
+        {
+            _itemDescription.text = itemSlot?.CurrentItem.Description;
+        }
+        
+        private void HideDescription()
+        {
+            _itemDescription.text = "";
+        }
+
+        private void OnTabActivate(UIMenuTabType tabType)
+        {
+            if (_tabType == tabType)
+            {
+                _inventoryUI.SetActive(true);
+            }
+            else
+            {
+                _inventoryUI.SetActive(false);
+            }
+        }
+    }
+}
