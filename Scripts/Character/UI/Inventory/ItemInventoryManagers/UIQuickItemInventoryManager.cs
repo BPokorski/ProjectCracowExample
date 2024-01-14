394b11e046dddf19b9ce7868ee46dc4c1f2ddb1b
BPokorski <51976955+BPokorski@users.noreply.github.com>
2024-01-14 19:01:43 +0100

    Add files via upload

---
+++ Scripts/Character/UI/Inventory/ItemInventoryManagers/UIQuickItemInventoryManager.cs
@@ -0,0 +1,80 @@
+﻿using Character.Data;
+using Character.Inventory;
+using Character.UI.Inventory.ItemSlots;
+using Equipment;
+using Events.EventArgs.UI;
+using UnityEngine;
+using Zenject;
+
+namespace Character.UI.Inventory.ItemInventoryManagers
+{
+    public class UIQuickItemInventoryManager : UIBaseItemInventoryManager
+    {
+        [Header("Slot data")]
+        [SerializeField] private ItemSlotsData _itemSlotsData;
+
+        [Inject]
+        private void Construct(UIQuickItemSlot.Factory quickItemSlotFactory)
+        {
+            _factory = quickItemSlotFactory;
+        }
+        
+        protected override void Awake()
+        {
+            base.Awake();
+            InitItems(_itemSlotsData.QuickSlotNumber);
+            InitItemSlots();
+        }
+
+        private void OnEnable()
+        {
+            foreach (var itemSlot in _items)
+            {
+                itemSlot.ItemSlotActionTrigger += UpdateQuickItemSlot;
+            }
+            
+            var items = GetItems();
+            ShowItems(items);
+        }
+
+        private void OnDisable()
+        {
+            foreach (var itemSlot in _items)
+            {
+                itemSlot.ItemSlotActionTrigger -= UpdateQuickItemSlot;
+            }
+            HideItems();
+        }
+        
+        private ItemSlot[] GetItems()
+        {
+            return _inventory.QuickItems;
+        }
+        
+        private void UpdateQuickItemSlot(ItemSlotEventArgs itemSlotEventArgs)
+        {
+            var currentItemSlot = itemSlotEventArgs.ItemSlot;
+            var quickSlotID = itemSlotEventArgs.ItemSlotId;
+            if (currentItemSlot != null)
+            {
+                var quickSlotPosition = _itemSlotIDPosition[quickSlotID];
+                // Debug.Log($"{quickSlotID}: pos: {quickSlotPosition}");
+                _inventory.SetQuickSlotItem(quickSlotPosition, currentItemSlot);
+
+                for (int i = 0; i < _items.Length; i++)
+                {
+                    var quickItem = _items[i];
+
+                    var quickItemItemSlot = quickItem.CurrentItemSlot;
+                    if (quickItemItemSlot != null && 
+                        quickItemItemSlot.CurrentItem.ItemID == currentItemSlot.CurrentItem.ItemID &&
+                        i != quickSlotPosition)
+                    {
+                        quickItem.CurrentItemSlot = null;
+                        _inventory.RemoveQuickSLotItem(i);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Items/Item.cs
@@ -0,0 +1,28 @@
+﻿using Settings;
+using UnityEngine;
+
+namespace Items
+{
+    public abstract class Item : ScriptableObject
+    {
+        [field: SerializeField][field: ReadOnly] public int ItemID { get; private set; } = 1;
+        [field: SerializeField] public string ItemName { get; protected set; }
+        [field: TextArea(15,15)]
+        [field: SerializeField] public string Description { get; protected set; }
+        [field: SerializeField] public Sprite ItemIcon { get; protected set; }
+        [field: SerializeField] public GameObject ItemPrefab { get; protected set; }
+        [field: SerializeField] public ItemType ItemType { get; protected set; }
+        [field: SerializeField] public bool IsRemovable { get; protected set; } = true;
+
+        private static int _currentMaxInstanceId = 1;
+
+
+        protected Item()
+        {
+            ItemID = _currentMaxInstanceId;
+            _currentMaxInstanceId += 1;
+        }
+    }
+    
+    
+}
