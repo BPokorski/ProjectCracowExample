ad943c224464b404f6c676729a6d4b7b0ccf94ff
BPokorski <51976955+BPokorski@users.noreply.github.com>
2024-01-24 19:16:18 +0100

    Add files via upload

---
+++ Scripts/Character/Fight/BaseFightManager.cs
@@ -0,0 +1,30 @@
+﻿using Equipment;
+using Inputs;
+using Stats.PlayerStats;
+using UnityEngine;
+using Zenject;
+
+namespace Character.Fight
+{
+    public abstract class BaseFightManager : MonoBehaviour
+    {
+        [Header("Stamina Manager and Stamina Cost")]
+        [SerializeField] protected StaminaManager _staminaManager;
+        [SerializeField] protected StaminaActionCost _baseAttackStaminaCost;
+        
+        [Header("Equipped Weapon")]
+        [SerializeField] protected EquippedWeapon _equippedWeapon;
+        
+        protected IAnimationManager _animationManager;
+        protected IInputsManager _inputs;
+        
+        [Inject]
+        private void Construct(IAnimationManager animationManager, IInputsManager inputs)
+        {
+            _animationManager = animationManager;
+            _inputs = inputs;
+        }
+        
+        
+    }
+}
